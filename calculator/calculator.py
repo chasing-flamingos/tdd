23c6fc363d29a424ba74fa57e71a2abe0eaa13cd
Katarzyna Zielińska <katarzyna9876@op.pl>
2019-03-13 23:01:21 +0100

    Implement substract()

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -3,3 +3,6 @@ def add(a, b):
 
 def negative(a):
     return -a
+
+def substract(a, b):
+    return add(a, negative(b))
--- calculator/calculator_test.py
+++ calculator/calculator_test.py
@@ -11,5 +11,11 @@ class TestCalculator(unittest.TestCase):
         self.assertEqual(3, calculator.negative(-3))
         self.assertEqual(0, calculator.negative(0))
 
+    def test_substract(self):
+        self.assertEqual(2, calculator.substract(6, 4))
+        self.assertEqual(6, calculator.substract(2, -4))
+        self.assertEqual(-4, calculator.substract(-2, 2))
+        self.assertEqual(0, calculator.substract(-2, -2))
+
 if __name__ == '__main__':
     unittest.main()
