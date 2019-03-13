e1babf5e51fff192b5b25de1f844af677563a4f0
Katarzyna Zielińska <katarzyna9876@op.pl>
2019-03-13 22:53:52 +0100

    Implement calculator.negative()

--- calculator/calculator_test.py
+++ calculator/calculator_test.py
@@ -6,5 +6,10 @@ class TestCalculator(unittest.TestCase):
     def test_add(self):
         self.assertEqual(3, calculator.add(1, 2))
 
+    def test_negative(self):
+        self.assertEqual(-3, calculator.negative(3))
+        self.assertEqual(3, calculator.negative(-3))
+        self.assertEqual(0, calculator.negative(0))
+
 if __name__ == '__main__':
     unittest.main()
