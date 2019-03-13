dbf6335600fc90169c342026098669211013e273
Katarzyna Zielińska <katarzyna9876@op.pl>
2019-03-13 23:02:53 +0100

    Reorganize functions order

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -1,8 +1,8 @@
-def add(a, b):
-    return a + b
-
 def negative(a):
     return -a
+
+def add(a, b):
+    return a + b
 
 def substract(a, b):
     return add(a, negative(b))
--- calculator/calculator_test.py
+++ calculator/calculator_test.py
@@ -3,13 +3,13 @@ import calculator
 
 class TestCalculator(unittest.TestCase):
 
-    def test_add(self):
-        self.assertEqual(3, calculator.add(1, 2))
-
     def test_negative(self):
         self.assertEqual(-3, calculator.negative(3))
         self.assertEqual(3, calculator.negative(-3))
         self.assertEqual(0, calculator.negative(0))
+
+    def test_add(self):
+        self.assertEqual(3, calculator.add(1, 2))
 
     def test_substract(self):
         self.assertEqual(2, calculator.substract(6, 4))
