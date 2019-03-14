5875d420877e8ddb9311cd94624b56491a921df2
Katarzyna Zielińska <katarzyna9876@op.pl>
2019-03-14 14:18:57 +0100

    Add RomanNumeral class

---
+++ roman_numerals/roman_numeral.py
@@ -0,0 +1,4 @@
+class RomanNumeral:
+
+    def __str__(self):
+        return ''
---
+++ roman_numerals/roman_numeral_test.py
@@ -0,0 +1,10 @@
+import unittest
+from roman_numeral import RomanNumeral
+
+class TestRomanNumeral(unittest.TestCase):
+
+    def test_default_value_is_null(self):
+        self.assertEqual('', str(RomanNumeral()))
+
+if __name__ == '__main__':
+    unittest.main()
